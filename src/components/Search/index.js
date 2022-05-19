22b807f3807a08742f7952806bd3706b5f6559dd
Justin Westra <justin.westra@genpact.digital>
2022-05-19 11:39:36 -0400

    Update comments

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,10 +1,16 @@
 import React, { useState, useDeferredValue, useMemo } from 'react';
 import { SearchResults } from 'components/SearchResults';
 
+// Deferred value demo
+// https://reactjs.org/docs/hooks-reference.html#usedeferredvalue
 export const Search = () => {
   const [query, setQuery] = useState('');
+
+  // Creates essentially a "deferred piece of state" for triggering renders
   const deferredQuery = useDeferredValue(query);
   const handleChange = ({ target: { value } }) => setQuery(value);
+
+  // Memoizing prevents children from re-rendering during urgent updates
   const results = useMemo(() => <SearchResults query={deferredQuery} />, [deferredQuery]);
   return (
     <>
